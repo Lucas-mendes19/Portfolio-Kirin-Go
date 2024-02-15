ebcf61fe060d6b42e854a0df10cf49485b0b7913
Lukelt <lucas.mendes.carvalho14@gmail.com>
2024-02-15 20:17:33 -0300

    feat: initial handle

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,74 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"github.com/Lucas-mendes19/Portfolio-Kirin-Go/configs"
+	"github.com/Lucas-mendes19/Portfolio-Kirin-Go/internal/domain/entity"
+	"github.com/Lucas-mendes19/Portfolio-Kirin-Go/internal/domain/repository"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"net/http"
 )
 
 func main() {
-	env, _ := configs.Load(".")
+	_, err := configs.Load(".")
+	if err != nil {
+		panic(err)
+	}
 
-	fmt.Println(env.ServerPort)
-	fmt.Println(env.Database.Port)
+	db, err := gorm.Open(sqlite.Open("database.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	err = db.AutoMigrate(&entity.User{}, &entity.Playlist{})
+	if err != nil {
+		return
+	}
+
+	playlistRepo := repository.NewPlaylistRepository(db)
+	playlistHandler := NewPlaylistHandler(playlistRepo)
+
+	http.HandleFunc("/playlists", playlistHandler.CreatePlaylist)
+	err = http.ListenAndServe(":8081", nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+type PlaylistDTO struct {
+	Title string `json:"title"`
+}
+
+type PlaylistHandler struct {
+	PlaylistDB repository.PlaylistInterface
+}
+
+func NewPlaylistHandler(db repository.PlaylistInterface) *PlaylistHandler {
+	return &PlaylistHandler{
+		PlaylistDB: db,
+	}
+}
+
+func (handler *PlaylistHandler) CreatePlaylist(w http.ResponseWriter, r *http.Request) {
+	var dto PlaylistDTO
+	err := json.NewDecoder(r.Body).Decode(&dto)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	playlist, err := entity.CreatePlaylist(dto.Title)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	err = handler.PlaylistDB.Create(playlist)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
 }
